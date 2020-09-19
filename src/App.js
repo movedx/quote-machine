f6f96ba5f917383735383c2e506116374beae48e
movedx <grytsenko.bogdan@gmail.com>
2020-09-19 16:47:55 +0200

    bootstrap styles and twitter button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import QuoteBox from "./components/QuoteBox";
 
 const App = () => {
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+
+  //   script.src =
+  //     "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+  //   script.async = true;
+
+  //   document.body.appendChild(script);
+
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
   return (
     <div>
       <QuoteBox />
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -14,22 +14,38 @@ const QuoteBox = () => {
   const author = useSelector(selectAuthor);
   const dispatch = useDispatch();
   return (
-    <div id="quote-box">
-      <div id="text">{quote}</div>
-      <div id="author">{author}</div>
-      <div id="new-quote">
-        <button onClick={() => dispatch(getRandomQuote())}>New Quote</button>
+    <div
+      id="quote-box"
+      className="card"
+      style={{ width: "18rem", margin: "auto", marginTop: "100px" }}
+    >
+      <div className="card-body">
+        <h6 id="author" className="card-subtitle mb-2 text-muted">
+          {author}
+        </h6>
+        <p id="text" className="card-text">
+          {quote}
+        </p>
+        <button
+          id="new-quote"
+          type="button"
+          className="btn btn-outline-dark card-link"
+          onClick={() => dispatch(getRandomQuote())}
+        >
+          New Quote
+        </button>
+        <a
+          className="btn btn-social-icon btn-twitter card-link"
+          id="tweet-quote"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            quote + " - " + author
+          )}`}
+        >
+          <span class="fa fa-twitter"></span>
+        </a>
       </div>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-          quote + " - " + author
-        )}`}
-        id="tweet-quote"
-      >
-        Tweet quote
-      </a>
     </div>
   );
 };
