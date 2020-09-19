503c2761cc3321a24d3d5712c4c6a4624132f465
movedx <grytsenko.bogdan@gmail.com>
2020-09-19 18:33:02 +0200

    Updates

---
+++ precache-manifest.768704526c27174794ebb098d89fd600.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a947b96aba65f0032846763d8c43f9",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "e2a73c0c16120921c0d1",
+    "url": "/quote-machine/static/css/2.22f1d8d3.chunk.css"
+  },
+  {
+    "revision": "71de569e22241ba3cfa2",
+    "url": "/quote-machine/static/css/main.8751c402.chunk.css"
+  },
+  {
+    "revision": "e2a73c0c16120921c0d1",
+    "url": "/quote-machine/static/js/2.891f62ef.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/quote-machine/static/js/2.891f62ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71de569e22241ba3cfa2",
+    "url": "/quote-machine/static/js/main.741d1c0d.chunk.js"
+  },
+  {
+    "revision": "256db52c5a0c9de87c09",
+    "url": "/quote-machine/static/js/runtime-main.6ade76f3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/quote-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/quote-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/quote-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/quote-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/quote-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
