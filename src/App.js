8ba75d2fe49f235c6658223f8379e8c90d070fc0
movedx <grytsenko.bogdan@gmail.com>
2020-09-19 17:14:39 +0200

    add freecodecamp tests

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { useEffect } from "react";
 import QuoteBox from "./components/QuoteBox";
 
 const App = () => {
-  // useEffect(() => {
-  //   const script = document.createElement("script");
+  useEffect(() => {
+    const script = document.createElement("script");
 
-  //   script.src =
-  //     "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
-  //   script.async = true;
+    script.src =
+      "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    script.async = true;
 
-  //   document.body.appendChild(script);
+    document.body.appendChild(script);
 
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div>
       <QuoteBox />
